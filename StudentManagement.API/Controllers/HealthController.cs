6e9434bd1a36af4edffa6c837ea2f11f906c81a9
AhmedHamdy3 <ahmed195053@feng.bu.edu.eg>
2026-04-18 20:57:16 +0200

    syntax error in health endpoint

--- StudentManagement.API/Controllers/HealthController.cs
+++ StudentManagement.API/Controllers/HealthController.cs
@@ -12,7 +12,7 @@ namespace StudentManagement.API.Controllers
         {
             return Ok(new
             {
-                Status = "Healthy",
+                Status = "Healthy"
                 Message = "Student Management API is running successfully."
             });
         }
