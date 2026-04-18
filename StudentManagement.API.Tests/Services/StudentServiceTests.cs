b3d30c52a17dc103a7143a77a5f7f1430e39d578
AhmedHamdy3 <ahmed195053@feng.bu.edu.eg>
2026-04-18 20:11:12 +0200

    Add test project

---
+++ StudentManagement.API.Tests/Services/StudentServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using StudentManagement.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.API.Tests.Services
+{
+    public class StudentServiceTests
+    {
+        [Fact]
+        public void GetAll_shouldReturnAllStudents()
+        {
+            var service = new StudentService();
+            var result = service.GetAll();
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal(2, result.Count);
+        }
+    }
+}
