396a48822ec3db98e48ca78e271c7f7ab80ffd94
AhmedHamdy3 <ahmed195053@feng.bu.edu.eg>
2026-04-18 17:41:41 +0200

    Add project files.

---
+++ StudentManagement.API/Program.cs
@@ -0,0 +1,45 @@
+
+using StudentManagement.API.Services;
+
+namespace StudentManagement.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                });
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Register application services
+            builder.Services.AddSingleton<IStudentService, StudentService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ StudentManagement.API/Services/StudentService.cs
@@ -0,0 +1,27 @@
+﻿using StudentManagement.API.Models;
+
+namespace StudentManagement.API.Services
+{
+    public class StudentService : IStudentService
+    {
+        private static readonly List<Student> Students = new()
+        {
+            new Student { Id = 1, Name = "Yassir Ibrahim", Email = "yassir@ibrahim.com", Course = "ASP.NET Core" },
+            new Student { Id = 2, Name = "Hamdy Gamal", Email = "hamdy@gamal.com", Course = "C#" }
+        };
+        public List<Student> GetAll()
+        {
+            return Students;
+        }
+        public Student? GetById(int id)
+        {
+            return Students.FirstOrDefault(x => x.Id == id);
+        }
+        public Student Add(Student student)
+        {
+            student.Id = Students.Max(x => x.Id) + 1;
+            Students.Add(student);
+            return student;
+        }
+    }
+}
